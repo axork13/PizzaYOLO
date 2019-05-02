cd80b17b59ef8c9de453400d69a0b3d2791d56e5
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-05-02 20:41:51 +0200

    Ajout du service Pizza

--- src/app/services/pizza.service.ts
+++ src/app/services/pizza.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { Pizza } from '../classes/pizza';
+
+import * as PizzaData from '../../data.json';
+import * as _ from 'lodash';
+import { IIngredient } from '../interfaces/ingredient';
 
 @Injectable()
 export class PizzaService {
 
-  
+  private pizzas: Pizza[] = [];
 
-  constructor() { }
+  constructor() {
+     (<any>PizzaData).pizzas.forEach(pizza => {
+       this.pizzas.push(new Pizza(pizza));
+     });
+  }
+
+  public createPizza(name: string, imageUrl: string, ingredients: IIngredient[], toppings: string[], prices: number[]) {
+    const newPizzaData = {
+      id: this.getNextId(),
+      name,
+      imageUrl,
+      ingredients: [...ingredients],
+      toppings: [...toppings],
+      prices: [...prices]
+    }
+
+    const newPizza = new Pizza(newPizzaData);
+
+    this.pizzas.push(newPizza);
+    return newPizza;
+  }
+
+  public getPizzas(): Pizza[] {
+    return this.pizzas;
+  }
+
+  public getPizzaById(id: number): Pizza {
+    return _.find(this.pizzas, (pizza) => pizza.id === id);
+  }
+
+  private getNextId(): number {
+    const max = _.maxBy(this.pizzas, (pizza) => pizza.id);
+    return max.id + 1;
+  }
 }
